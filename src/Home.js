e847261e0c565e64a8b2140cdbb81f5563cdb524
jeganpetchimuthu <127992705+Jeganpetchimuthu@users.noreply.github.com>
2023-09-14 12:28:49 +0530

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import image from "../../webcode/src/img/img9.png";
+import Nav from "./Navication/Nav";
+function Home() {
+  return (
+    <div>
+      <div>
+        <img src={image}></img>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
